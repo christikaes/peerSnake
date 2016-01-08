8cc235416f33e5e16e4da261e9dcdae60e7ad35d
Christina <christikaes@gmail.com>
2016-01-07 23:24:14 -0500

    update/draw

--- snake.js
+++ snake.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
 	console.log("Hello Snake");
+
+	FPS = 60;
+
+	setInterval(function(){
+		update();
+		draw();
+	});
+
+	var update = function(){
+		console.log("update");
+	}
+
+	var draw = function(){
+		console.log("draw")
+	}
+
+
 })
