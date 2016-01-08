34f21970b52d5000d38fd9065f0da532af7f606d
Christina <christikaes@gmail.com>
2016-01-07 23:49:46 -0500

    create game grid

--- snake.js
+++ snake.js
@@ -1,19 +1,77 @@
 $(document).ready(function(){
 	console.log("Hello Snake");
 
-	FPS = 60;
+	var canvasWidth = 500;
+	var canvasHeight = 500;
 
+	var canvas = $("#game")[0];
+	var context = canvas.getContext("2d");
+
+	var gameState = [
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	];
+
+	FPS = 60;
 	setInterval(function(){
 		update();
 		draw();
-	});
+	}, 1000/FPS);
 
 	var update = function(){
 		console.log("update");
+
 	}
 
 	var draw = function(){
 		console.log("draw")
+
+		context.clearRect(0,0,canvasWidth,canvasHeight)
+		context.fillStyle = "#eee";
+
+		var xLength = gameState[0].length;
+		var yLength = gameState.length;
+
+		console.log(xLength)
+		console.log(yLength)
+
+		var padding = 5;
+
+		var remX = canvasWidth - padding*(xLength-1);
+		var remY = canvasWidth - padding*(yLength-1);
+
+		console.log(remX)
+		console.log(remY)
+
+		var squareWidth = remX/xLength;
+		var squareHeight = remY/yLength;
+
+		console.log(squareWidth)
+
+		for(i = 0; i < xLength; i++){
+			for(j = 0; j < yLength; j++){
+				context.fillRect(i*(squareWidth + padding), j * (squareHeight + padding),squareWidth,squareHeight);
+			}
+		}
+
 	}
 
 
